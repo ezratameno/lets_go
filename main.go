5c79f1e549e24f0fa668831efdb6ad2d9434bba3
ezratameno <ezratameno@gmail.com>
2022-02-08 21:02:52 +0200

    tell the user which method create accepts

--- main.go
+++ main.go
@@ -30,11 +30,11 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 	// http.MethodPost is a constant equal to the string "POST".
 
 	if r.Method != http.MethodPost {
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
 
+		w.Header().Set("Allow", http.MethodPost)
 		w.WriteHeader(405)
 		w.Write([]byte("Method Not Allowed"))
 		return
