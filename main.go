6e2b1686a6af6f09a8d6de085441054ff43551c6
ezratameno <ezratameno@gmail.com>
2022-02-09 16:18:32 +0200

    add paramter to showSnippet handler

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -20,8 +22,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a showSnippet handler function.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response.
 
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Use the fmt.Fprintf() function to interpolate the id value with our response
+	// and write it to the http.ResponseWriter.
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
 }
 
 // Add a createSnippet handler function.
@@ -35,8 +49,8 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 		// the second parameter is the header value.
 
 		w.Header().Set("Allow", http.MethodPost)
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method Not Allowed", 405)
+
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
